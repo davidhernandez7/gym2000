01b7b3049eb8237426aebea4eba8f4468a7bcaf9
davidhernandez7 <114596085+davidhernandez7@users.noreply.github.com>
2025-03-19 16:09:36 -0600

    Cambios de nuevo proff

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,13 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-  // Para evitar múltiples instancias en desarrollo (Next.js reinicia mucho)
-  var prisma: PrismaClient | undefined
+  // Evita crear múltiples instancias en desarrollo
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient()
+const prisma = global.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
-export default prisma
+export default prisma;
